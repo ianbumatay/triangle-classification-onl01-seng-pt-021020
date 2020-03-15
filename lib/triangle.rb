7fd9f4acbb1732dbedc2e115ca1aa71d3b1d3cb1
Ian  Bumatay <ianbumatay.09@gmail.com>
2020-03-15 07:06:27 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,14 +3,13 @@ class Triangle
 
   attr_accessor :right_side, :left_side,:buttom
 
-  def initialize
-    @right_side = right_side
-    @left_side = left_side
-    @buttom = buttom
+  def initialize(name)
+    @name = name
+  
   end
 
   def kind(:equilateral)
-    if @right_side == @left_side  && @left_side != buttom
+    if right_side == left_side  && left_side != buttom
       raise TriangleError
     end
   end
