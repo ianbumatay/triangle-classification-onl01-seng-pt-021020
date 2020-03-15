51d0a51b05b8c4553547b71ce13295f99ffd0974
Ian  Bumatay <ianbumatay.09@gmail.com>
2020-03-15 06:25:19 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,19 @@
 class Triangle
   # write code here
+  def initialize :right_side, :left_side, :buttom
+    @right_side = right_side
+    @left_side = left_side 
+    @buttom = buttom
+  end 
+
+def kind()
+   if :equilateral => left_side != right_side && right_side != buttom
+      TriangleError
+   else
+     :equilateral => left_side == right_side && right_side == buttom
+   end
+ end 
+ 
+class TriangleError < StandardError
+  end
 end
